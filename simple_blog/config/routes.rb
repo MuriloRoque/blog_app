bf476ee946d9457f616594eada9989381714c9fe
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 08:05:30 -0300

    Create navbar with Bootstrap

--- simple_blog/config/routes.rb
+++ simple_blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get 'about' => 'pages#about'
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
   resources :posts
 end
