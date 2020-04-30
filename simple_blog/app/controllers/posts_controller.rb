f857475e90264e1d60a7d414244f115dd19a12e6
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 10:57:17 -0300

    Add edit and destroy methods

--- simple_blog/app/controllers/posts_controller.rb
+++ simple_blog/app/controllers/posts_controller.rb
@@ -21,6 +21,26 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private def post_params
     params.require(:post).permit(:title, :body)
   end
