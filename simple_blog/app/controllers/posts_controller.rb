0f5cac73ac4679d343a6c8f6fb1e9e7d76cf72dc
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 06:27:32 -0300

    create new model and database

--- simple_blog/app/controllers/posts_controller.rb
+++ simple_blog/app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def index
     
   end
+
+  def new
+
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    
+  end
+
 end
--- simple_blog/config/routes.rb
+++ simple_blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'about' => 'pages#about'
+  resources :posts
 end
