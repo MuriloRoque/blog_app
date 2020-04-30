1ff6398cde488e6818e5f626b5d9ada479bdc452
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 11:39:22 -0300

    Add authentication settings and more design

--- simple_blog/app/controllers/comments_controller.rb
+++ simple_blog/app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "murilo", password: "1234", only: [:destroy]
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
--- simple_blog/app/controllers/posts_controller.rb
+++ simple_blog/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "murilo", password: "1234", except: [:index, :show]
+  
   def index
     @posts = Post.all
   end
