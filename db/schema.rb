569a6f7d89f29006ffadbb4e4a401e11e7ba69d1
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 12:18:40 -0300

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_135820) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "username"
-    t.text "body"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+ActiveRecord::Schema.define(version: 202_004_301_358_20) do
+  create_table 'comments', force: :cascade do |t|
+    t.string 'username'
+    t.text 'body'
+    t.integer 'post_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
