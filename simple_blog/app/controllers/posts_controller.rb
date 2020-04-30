d1909cd112a6e71264dd94637d4a247bc5656dba
Murilo Roque <muriloengqui@gmail.com>
2020-04-30 06:56:38 -0300

    edited controller methods to save posts

--- simple_blog/app/controllers/posts_controller.rb
+++ simple_blog/app/controllers/posts_controller.rb
@@ -3,13 +3,22 @@ class PostsController < ApplicationController
     
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
-    # render plain: params[:post].inspect
-    
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
 
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
